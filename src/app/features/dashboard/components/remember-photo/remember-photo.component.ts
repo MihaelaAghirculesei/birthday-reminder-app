93c022f090c987c01626d4a09181e1348ce3ffba
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-12-15 16:11:53 +0100

    refactor(dashboard): improve remember-photo component

--- src/app/features/dashboard/components/remember-photo/remember-photo.component.ts
+++ src/app/features/dashboard/components/remember-photo/remember-photo.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from '../../../../shared';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-remember-photo',
   standalone: true,
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MatTooltipModule],
   templateUrl: './remember-photo.component.html',
   styleUrls: ['./remember-photo.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
