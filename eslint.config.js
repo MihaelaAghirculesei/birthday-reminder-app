c17840705d67fb13a1a01a21968c4136bfe9a3ae
MihaelaAghirculesei <aghirculesei@gmail.com>
2025-12-19 07:50:24 +0100

    chore: configure ESLint rules for Angular 17 compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -31,6 +31,8 @@ module.exports = defineConfig([
           style: "kebab-case",
         },
       ],
+      "@angular-eslint/prefer-inject": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
   {
